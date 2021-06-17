16e1223f0e2b0f6441d3368aec4886f09cfd973b
Sytse Oegema <sy.oegema@oegematransport.nl>
2021-06-17 08:25:13 +0200

    Added function for multiple images in a singal request.

--- functions/run.sh
+++ functions/run.sh
@@ -2,6 +2,9 @@
 gcloud functions deploy image-upload --entry-point MoodCap.PostImage --runtime dotnet3 --trigger-http --allow-unauthenticated
 # upload the image-list function
 gcloud functions deploy image-list --entry-point MoodCap.ListImages --runtime dotnet3 --trigger-http --allow-unauthenticated
+# upload the images-upload function
+gcloud functions deploy images-upload --entry-point MoodCap.PostImages --runtime dotnet3 --trigger-http --allow-unauthenticated
+
 
 # upload the data-upload function
 ### this function has been used for testing purpose only
